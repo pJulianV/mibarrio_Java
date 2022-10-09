65baf5b33dfd31c352e3314822f39347e5d7d101
Julian Vargas <julianvargastrb@gmail.com>
2022-10-08 22:36:29 -0500

    Conectar con mysql

--- InitializrSpringbootProject/src/main/java/com/tiendavirtual/mibarrio/MibarrioApplication.java
+++ InitializrSpringbootProject/src/main/java/com/tiendavirtual/mibarrio/MibarrioApplication.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
 @SpringBootApplication
+@RestController
 public class MibarrioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MibarrioApplication.class, args);
 	}
+         
+        @RequestMapping(value="/hola")
+        public String index(){
+            return "Hola Mundo";}
+        
+        
 
 }
